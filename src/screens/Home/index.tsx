44516fdb39212d24c00b37f2870620a5a0036f2f
lucasdmandrade <lucasdmandrade@gmail.com>
2022-08-08 23:23:27 -0300

    feat(ust1) Create project (App)

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { StatusBar, View, Text } from "react-native";
+import { Container } from "./styles";
+
+export default function Home() {
+    return (
+      <Container >
+        <Text>Home</Text>
+        <StatusBar  />
+      </Container>
+    );
+  }
+  
+  
